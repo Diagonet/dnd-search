4f288b7dc5a8f9c9ac0a565abc201aa4bf721366
Erik Tabs <erik.teruaki@gmail.com>
2021-06-10 23:07:02 -0300

    inital commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import axios from "axios";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+class Search extends React.Component {
+  render() {
+    return (
+      <div>
+        <input type="text"></input>
+        <button>Search</button>
+      </div>
+    );
+  }
+}
+ReactDOM.render(<Search />, document.getElementById("root"));
