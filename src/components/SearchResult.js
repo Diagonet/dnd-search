ad572d293d63019c0f5ee6a544072b0e47dd4521
Erik Tabs <erik.teruaki@gmail.com>
2021-06-16 01:42:34 -0300

    first page attempt

--- src/components/SearchResult.js
+++ src/components/SearchResult.js
@@ -2,16 +2,25 @@ import React from "react";
 export default class SearchResult extends React.Component {
   render() {
     return (
-      <div className="search-result">
+      <div
+        className={[
+          "search-result",
+          this.props.value.concentration && "concentration",
+        ].join(" ")}
+      >
         <h2>{this.props.value.name}</h2>
         <div>
           <b>Range:</b> {this.props.value.range}
         </div>
         <div>
-          <b>Description:</b> {this.props.value.desc}
+          <b>Duration:</b> {this.props.value.duration}
+        </div>
+        <div>
+          <b>Description:</b>
+          <br />
+          {this.props.value.desc}
         </div>
       </div>
     );
   }
 }
-
