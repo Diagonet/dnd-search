f0b728c0bee9e4f751d4a061b4bd5e9fa173449d
Erik Tabs <erik.teruaki@gmail.com>
2021-06-25 00:54:32 -0300

    minor changes to appearance and set a maximum width to look better on phones

--- src/components/SingleResult.js
+++ src/components/SingleResult.js
@@ -1,4 +1,5 @@
 import { CSSTransition } from "react-transition-group";
+import React from "react";
 
 const SingleResult = ({ value, onClickFunction, displayFlag, animations }) => {
   console.log(animations);
